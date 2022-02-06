4ed5b951764d030ae37d652b5783b691171616f3
linzaixiong <linzaixiong1@rainbowcn.com>
2022-02-06 21:54:47 +0800

    feat：增加alibaba nacos注册中心，并配置

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -3,9 +3,11 @@ package com.atguigu.gulimall.member;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.atguigu.gulimall.member.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimallMemberApplication {
     public static void main(String[] args) {
         SpringApplication.run(GulimallMemberApplication.class, args);
