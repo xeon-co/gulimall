bb4a2f3ea1b865cfb1d707c8e32810a08db90a6b
linzaixiong <linzaixiong1@rainbowcn.com>
2022-02-07 10:12:19 +0800

    feat：member用feign远程调用coupon

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -4,8 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @MapperScan("com.atguigu.gulimall.member.dao")
+@EnableFeignClients(basePackages = "com.atguigu.gulimall.member.feign")
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GulimallMemberApplication {
