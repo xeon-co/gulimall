1b04d36003259c2850999c50e0b59a8ed4a5fc05
linzaixiong <linzaixiong1@rainbowcn.com>
2022-02-20 21:43:33 +0800

    feat：分类增加追加和删除按钮

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -2,7 +2,31 @@
 <template>
     <div>
         <h1>三级分类维护</h1>
-        <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+        <el-tree :data="data" 
+        :props="defaultProps" 
+        show-checkbox 
+        node-key="catId"
+        :expand-on-click-node="false">
+          <span class="custom-tree-node" slot-scope="{ node, data }">
+          <span>{{ node.label }}</span>
+          <span>
+            <el-button
+              v-if="node.level <= 2"
+              type="text"
+              size="mini"
+              @click="() => append(data)">
+              Append
+            </el-button>
+            <el-button
+              v-if="node.childNodes.length == 0"
+              type="text"
+              size="mini"
+              @click="() => remove(node, data)">
+              Delete
+            </el-button>
+          </span>
+        </span>
+        </el-tree>
     </div>
 </template>
 
@@ -20,6 +44,13 @@
     methods: {
       handleNodeClick(data) {
         console.log(data);
+      },
+      append(data){
+        console.log("数据append:", data)
+      },
+      remove(node, data){
+        console.log("数据remove node:", node)
+        console.log("数据remove data:", data)
       },
       getMenu(){
         this.$http({
