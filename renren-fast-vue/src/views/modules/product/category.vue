a5e2b1879907a0d5f575a10ea21e3b30b5727dcc
linzaixiong <linzaixiong1@rainbowcn.com>
2022-02-20 13:29:35 +0800

    update：增加目录 商品系统-分类

---
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -0,0 +1,45 @@
+<!-- # https://element.eleme.cn/#/en-US/component/tree -->
+<template>
+    <div>
+        <h1>三级分类维护</h1>
+        <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        data: [],
+        defaultProps: {
+          children: 'children',
+          label: 'label'
+        }
+      };
+    },
+    methods: {
+      handleNodeClick(data) {
+        console.log(data);
+      },
+      getMenu(){
+        this.$http({
+            url: this.$http.adornUrl("/product/category/list/tree"),
+            method: "get"
+            }
+        ).then((data)=>{
+            console.log("数据:"+data)
+            }
+        );
+      }
+    },
+    created() {
+        this.getMenu();
+    }
+  };
+
+ 
+</script>
+
+<style>
+    
+</style>
